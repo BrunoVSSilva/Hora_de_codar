2d293454f43a1849359e7f94a0204cf79969c8e1
Bruno_Vicente_S_SILVA <103516597+BrunoVSSilva@users.noreply.github.com>
2022-08-31 18:31:27 -0300

    Questões realizadas :)

---
+++ Horadecodar1.js
@@ -0,0 +1,94 @@
+/* 1 Escreva um programa em Javascript e crie uma variável chamada "nomeDoCarro" e 
+atribua-a um valor "Fusca".*/
+
+/* 2 Escreva um programa em Javascript em que o usuário informe o seu nome e exiba a 
+mensagem "Olá, [NomeDoUsuario]".*/
+
+/* 3 Escreva um programa em Javascript em que o usuário informe o seu nome e em seguida 
+o programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem 
+"Olá, [NomeDoUsuario], sua idade é [idade]".*/
+
+/* Considerando a figura acima, escreva um programa para cada forma que calcule e exiba
+em tela cada uma de suas respectivas áreas. O usuário irá informar os valores de cada 
+variável. Crie um arquivo para cada fórmula e poste os  arquivos no GitHub. 
+Link do repositório abaixo.*/
+
+/* 4 Considerando a figura acima, escreva um programa para cada forma que calcule e 
+exiba em tela cada uma de suas respectivas áreas. O usuário irá informar os valores 
+de cada variável. Crie um arquivo para cada fórmula e poste os  arquivos no GitHub.
+Link do repositório abaixo. */
+
+
+
+
+/* Resposta 1 e 2
+var nomeDoCarro = "Fusca";
+var nome = prompt("Insira seu nome ");
+alert("Olá" + " " + nome)
+*/
+
+
+/* Resposta 3
+var nome = prompt("Insira seu nome");
+var idade = prompt("Insira sua idade");
+alert("Olá, " + nome + " sua idade é " + idade);
+*/
+
+
+/* Resposta 4*/
+
+/* Retângulo */
+/*
+var h =parseInt (prompt("Insira a altura"));
+var b =parseInt (prompt("Insira a base"));
+alert(b * h);
+/*
+
+/* Quadrado */
+/*
+var l1 =parseInt (prompt("Insira o Lado 1"));
+var l2 =parseInt (prompt("Insira o Lado 2"));
+alert(l1 * l2);
+/*
+
+/* Losango */
+/*
+var dmaior =parseInt (prompt("Insira a diagonal maior"));
+var dmenor =parseInt (prompt("Insira a diagonal menor"));
+alert(dmaior * dmenor / 2)
+*/
+
+/* Trapézio */
+/*
+var bmaior =parseInt(prompt("insira a base maior"));
+var bmenor =parseInt(prompt("insira a base menor"));
+var h =parseInt(prompt("Insira a altura"))
+var b = bmaior + bmenor;
+alert(b * h / 2);
+*/
+
+/* Paralelogramo */
+/*
+var h =parseInt (prompt("Insira a altura"));
+var b =parseInt (prompt("Insira a base"));
+alert(b * h);
+*/
+
+/* Triângulo */
+/*
+var h = parseInt(prompt("Insira a altura"));
+var b = parseInt(prompt("insira a base"));
+alert(h * b / 2);
+*/
+
+/* Círculo*/
+/*
+var raio =parseInt(prompt("Insira o raio"));
+var r2 = raio*raio
+alert(r2 * 3.1415)
+*/
+
+/* testando o console.log :) Um beijo professor!
+console.log("BOO!");
+*/
+
